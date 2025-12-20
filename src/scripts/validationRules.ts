d41b7ce186a778f15ab25d8c65cfc3ec1703c5fd
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-12-20 18:56:45 +0700

    feat: add passwordRules in validationRules.ts

--- src/scripts/validationRules.ts
+++ src/scripts/validationRules.ts
@@ -20,3 +20,26 @@ export const loginRules = [
     return 'Логин может содержать только латинские символы, _, - и цифры!'
   },
 ]
+
+export const passwordRules = [
+  (value: string) => {
+    if (!!value.trim()) return true
+
+    return 'Пароль не должен быть пустым!'
+  },
+  (value: string) => {
+    const len = value.trim().length
+
+    if (len > 7 && len < 73) return true
+
+    return 'Длина пароля от 8 до 72 символов!'
+  },
+  (value: string) => {
+    const regexPassword1: RegExp = new RegExp('^[a-zA-Z0-9`=!@#$%^&*()_+№;:?\\-\\\\/|]{8,72}$')
+    const password2 = value.trim()
+
+    if (regexPassword1.test(password2)) return true
+
+    return 'Разрешены только: a-z, A-Z, 0-9, `=!@#$%^&*()_+№;:?\-\\/|!'
+  },
+]
